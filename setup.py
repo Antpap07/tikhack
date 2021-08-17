4ad54aaff5c51ddec98c6d75bd0c088b6d7cc496
Antpap07 <86918172+Antpap07@users.noreply.github.com>
2021-08-17 22:48:16 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import.os
+import.sys
+
+# Versions
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+
+# Remove existing downloads and binaries so we can start from scratch.
+os.system("sudo apt-get remove google-chrome-stable")
+
+# Install dependencies.
+os.system("sudo apt-get update")
+os.system("sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4")
+
+# Install Chrome.
+os.system("sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add")
+os.system("sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list")
+os.system("sudo apt-get -y update")
+os.system("sudo apt-get -y install google-chrome-stable")
