1eebc1dc57be1201e3646b4cc8afc40b1f323ff8
Antpap07 <86918172+Antpap07@users.noreply.github.com>
2021-08-18 19:28:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,5 +21,13 @@ os.system("sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >>
 os.system("sudo apt-get -y update")
 os.system("sudo apt-get -y install google-chrome-stable")
 
+# fetching drivers for the new prev release of geckodrivers
+os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.{}.{}/geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,first,second,OS_bit))
+os.system("tar -xvzf geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,OS_bit))
+os.system("rm geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,OS_bit))
+os.system("chmod +x geckodriver")
+os.system("mv geckodriver /usr/local/bin/")
+os.system("chmod +x tikhack && chmod +x setup.py")
+
 # Give permission to run.
 os.system("chmod +x tikhack.py")
