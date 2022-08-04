0652d8646abca0e51da8a19f3a5602df6fe6dc8c
Antpap07 <86918172+Antpap07@users.noreply.github.com>
2022-08-04 12:33:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,42 @@
-#!/usr/bin/python
 import os, math, sys
 
-# Versions
-CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+#OS_bit = (round(math.log(sys.maxint,2)+1))  # get the bit
 
-# Remove existing downloads and binaries so we can start from scratch.
-os.system("sudo apt-get remove google-chrome-stable")
-
-# Install dependencies.
-os.system("sudo apt-get update")
-os.system("sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4")
-os.system("sudo apt-get install python-pip && sudo apt-get install tor")   
+os.system("sudo apt-get install python-pip && sudo apt-get install tor")   # installing dependencies
 os.system("pip install -U selenium")
 os.system("pip install Pysocks")
 os.system("pip install pyvirtualdisplay && apt-get install xvfb")
 
-# Install Chrome.
-os.system("sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add")
-os.system("sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list")
-os.system("sudo apt-get -y update")
-os.system("sudo apt-get -y install google-chrome-stable")
+#print("\n \n {} \n \n".format(OS_bit))
 
-# fetching drivers for the new prev release of geckodrivers
+
+os.system('firefox -v > tmp')                  # store result of firefox -v in tmp
+result   =  open('tmp', 'r').read()            # result var reads the output
+marker   = result.find('Firefox') + 8          # marker marks the 8th letter from the word "Firefox"
+     # spliting the output, the version is something like aa.bb.cc
+a,b,c = version.split(".")                     # a is the var with the aa
+os.remove('tmp')                               # removing the temporary file
+
+FirefoxVersion = int(a)
+second = 0
+
+if FirefoxVersion  < 53:
+
+    first = 16
+    second = 1
+    OS_bit = 64
+
+elif FirefoxVersion == 53 or FirefoxVersion == 54:
+
+    first = 18
+
+elif FirefoxVersion > 54:
+
+    first = 19
+
 os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.{}.{}/geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,first,second,OS_bit))
 os.system("tar -xvzf geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,OS_bit))
 os.system("rm geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,OS_bit))
 os.system("chmod +x geckodriver")
 os.system("mv geckodriver /usr/local/bin/")
-os.system("chmod +x tikhack && chmod +x setup.py")
-
-# Give permission to run.
-os.system("chmod +x tikhack.py")
+os.system ("chmod +x faitagram && chmod +x setup.py")
